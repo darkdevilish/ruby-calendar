588b2fec70130057ba58a4af536061407d10856c
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-28 03:22:07 -0400

    Adds day classes class and refactors calendar to its own class

--- calendar.rb
+++ calendar.rb
@@ -2,6 +2,49 @@ require 'active_support/core_ext/date'
 require 'active_support/core_ext/time'
 require 'active_support/core_ext/array'
 
+class Calendar
+  def initialize(date=Date.today)
+   @date = date
+  end
+
+  def to_a
+    CalendarMonthWeeks.new(@date).to_a.map do |week|
+      week.map do |date|
+        [date, DayStyles.new(date).to_s]
+      end
+    end
+  end
+
+end
+
+class DayStyles
+  def initialize(date)
+    @date = date
+  end
+
+  def to_s
+    [past, today, future, other_month].compact.join("  ")
+  end
+
+private
+
+  def past
+    "past" if @date < Date.today
+  end
+
+  def today
+    "today" if @date == Date.today
+  end
+
+  def future
+    "future" if @date > Date.today
+  end
+
+  def other_month
+    "other_month" if @date.month != Date.today.month
+  end
+end
+
 class CalendarMonthWeeks
   def initialize(date=Date.today)
     @date = date
