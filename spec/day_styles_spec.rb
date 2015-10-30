7db27663afdd3e252429d5329a638d9b848f1824
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-29 22:03:37 -0400

    Updates DayStyle spec file

--- spec/day_styles_spec.rb
+++ spec/day_styles_spec.rb
@@ -1,7 +1,6 @@
 require 'active_support/core_ext/date'
 require 'active_support/core_ext/time'
 require 'active_support/core_ext/array'
-require '../calendar_month_weeks'
 require '../day_styles'
 
 describe DayStyles do
